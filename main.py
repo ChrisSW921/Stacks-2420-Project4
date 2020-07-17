13f5e11852b0a89cd657dfbd18d121816ee9658b
Chris <withscottc921@gmail.com>
2020-07-17 09:00:47 -0600

    Finished eval postfix function. Only cleanup remains

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import stack as st
 
 
-def in2post(exp):
+def in2post(expr):
+    if not isinstance(expr, str):
+        raise ValueError
     stack = st.Stack()
     postfix = ""
-    for item in exp:
+    for item in expr:
         if item in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            postfix += item
+            postfix += f"{item} "
         elif item == '(':
             stack.push(item)
         elif item in ['+', '-', '/', '*']:
             try:
                 while stack.top():
                     if stack.top() in ['+', '-'] and item in ['+', '-']:
-                        postfix += stack.top()
+                        postfix += f"{stack.top()} "
                         stack.pop()
                     elif stack.top() in ['*', '/'] and item in ['+', '-']:
-                        postfix += stack.top()
+                        postfix += f"{stack.top()} "
                         stack.pop()
                     elif stack.top() in ['*', '/'] and item in ['*', '/']:
-                        postfix += stack.top()
+                        postfix += f"{stack.top()} "
                         stack.pop()
                     elif stack.top() in ['+', '-'] and item in ['*', "/"]:
                         stack.push(item)
@@ -32,7 +34,7 @@ def in2post(exp):
         elif item == ')':
             try:
                 while stack.top() != '(':
-                    postfix += stack.top()
+                    postfix += f"{stack.top()} "
                     stack.pop()
                 else:
                     stack.pop()
@@ -40,18 +42,47 @@ def in2post(exp):
                 pass
     try:
         while stack.top():
-            postfix += stack.top()
+            postfix += f"{stack.top()} "
             stack.pop()
     except IndexError:
-        print("OK")
+        pass
     return postfix
+
+
+def eval_postfix(expr):
+    stacky_boi = st.Stack()
+    operands = ['+', '-', '/', '*']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for thing in expr:
+        if thing in numbers:
+            stacky_boi.push(thing)
+        elif thing in operands:
+            num2 = int(stacky_boi.pop())
+            num1 = int(stacky_boi.pop())
+            if thing == '+':
+                result = num1 + num2
+                stacky_boi.push(result)
+            elif thing == '-':
+                result = num1 - num2
+                stacky_boi.push(result)
+            elif thing == '*':
+                result = num1 * num2
+                stacky_boi.push(result)
+            elif thing == '/':
+                result = num1 / num2
+                stacky_boi.push(result)
+    return stacky_boi.pop()
 
 
 def main():
     with open("data.txt", "r") as file:
         for line in file:
-            print(f"Infix: {line}")
-            print(in2post(line))
+            print(f"Infix: {line}", end="")
+            print(f"postfix: {in2post(line)}")
+            raw_post = in2post(line)
+            raw_post.replace(" ", "")
+            print(f"answer: {float(eval_postfix(raw_post))}")
+            print("")
 
 
 main()
