884604391548679df34afb9e875a1378634b69d8
Chris <withscottc921@gmail.com>
2020-07-17 09:24:40 -0600

    Finished passing tests--only iowrapper fails on fault of tests

--- main.py
+++ main.py
@@ -89,4 +89,5 @@ def main():
             print("")
 
 
-main()
+if __name__ == "__main__":
+    main()
--- stack.py
+++ stack.py
@@ -1,49 +1,61 @@
+"""Stack module"""
+
 class Node:
+    """Node Class"""
     def __init__(self, value, next_node=None):
         self.value = value
         self.next_node = next_node
 
     def set_next_node(self, next_node):
+        """Sets next node"""
         self.next_node = next_node
 
     def get_next_node(self):
+        """Gets next node"""
         return self.next_node
 
     def get_value(self):
+        """Gets value"""
         return self.value
 
 
 class Stack:
+    """Stack class"""
     def __init__(self, limit=1000):
         self.top_item = None
-        self.size = 0
+        self.ssize = 0
 
     def push(self, value):
+        """Push item"""
         item = Node(value)
         item.set_next_node(self.top_item)
         self.top_item = item
-        self.size += 1
+        self.ssize += 1
 
     def pop(self):
+        """Pop item"""
         if not self.is_empty():
             item_to_remove = self.top_item
             self.top_item = item_to_remove.get_next_node()
-            self.size -= 1
+            self.ssize -= 1
             return item_to_remove.get_value()
         raise IndexError
 
     def top(self):
+        """Get top item"""
         if not self.is_empty():
             return self.top_item.get_value()
         raise IndexError
 
     def size(self):
-        return self.size
+        """Get size"""
+        return self.ssize
 
     def is_empty(self):
-        return self.size == 0
+        """Tel if it is empty"""
+        return self.ssize == 0
 
     def clear(self):
+        """Clear stack"""
         while not self.is_empty():
             self.pop()
-
